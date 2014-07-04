bb228cc7875299d0ecf1530d5088a27cb054a463
alchemyunited <mark.simchock@alchemyunited.com>
2014-07-03 22:35:38 -0400

    quick fixes

--- js/5xy.js
+++ js/5xy.js
@@ -34,71 +34,78 @@ function theClick(displayBlock, progressComplete, styleOpacity, styleCursor){
   displayBlock.style.display="block";
   setProgress(progressComplete, 100-progressComplete);
   styleOpacity.style.opacity="0.2";
-  // styleCursor.style.disabled=true;
+  //styleCursor.style.disabled=true;
   styleCursor.style.cursor="default";
 }
 
 var getStartedBtn = getStarted.getElementsByClassName('btn');
-getStartedBtn[0].addEventListener("click", function() {
+getStartedBtn[0].addEventListener("click", function(e) {
   if ( getStarted.style.opacity != "0.2" ){
     progressComplete.className = progressComplete.className + ' active';
     progressRemain.className = progressRemain.className  + ' active';
     theClick(beforeWeBegin, 0, getStarted, getStartedBtn[0]);
   } else {
+    e.preventDefault();
     return false;
   }
 },false);
 
 var beforeWeBeginBtn = beforeWeBegin.getElementsByClassName('btn');
-beforeWeBeginBtn[0].addEventListener("click", function() {
+beforeWeBeginBtn[0].addEventListener("click", function(e) {
    if ( beforeWeBegin.style.opacity != "0.2"){
     theClick(whyOne, 17, beforeWeBegin, beforeWeBeginBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    }
 },false);
 
 var whyOneBtn = whyOne.getElementsByClassName('btn');
-whyOneBtn[0].addEventListener("click", function() {
+whyOneBtn[0].addEventListener("click", function(e) {
    if ( whyOne.style.opacity != "0.2"){
     theClick(whyTwo, 33, whyOne, whyOneBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    }
 },false);
 
 var whyTwoBtn = whyTwo.getElementsByClassName('btn');
-whyTwoBtn[0].addEventListener("click", function() {
+whyTwoBtn[0].addEventListener("click", function(e) {
    if ( whyTwo.style.opacity != "0.2"){
     theClick(whyThree, 50, whyTwo, whyTwoBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    }
 },false);
 
 var whyThreeBtn = whyThree.getElementsByClassName('btn');
-whyThreeBtn[0].addEventListener("click", function() {
+whyThreeBtn[0].addEventListener("click", function(e) {
    if ( whyThree.style.opacity != "0.2"){
     theClick(whyFour, 67, whyThree, whyThreeBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    } 
 },false);
 
 var whyFourBtn = whyFour.getElementsByClassName('btn');
-whyFourBtn[0].addEventListener("click", function() {
+whyFourBtn[0].addEventListener("click", function(e) {
    if ( whyFour.style.opacity != "0.2"){
     theClick(whyFive, 83, whyFour, whyFourBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    }
 },false);
 
 var whyFiveBtn = whyFive.getElementsByClassName('btn');
-whyFiveBtn[0].addEventListener("click", function() {
+whyFiveBtn[0].addEventListener("click", function(e) {
    if ( whyFive.style.opacity != "0.2"){
     theClick(congratsCompleted, 100, whyFive, whyFiveBtn[0]);
    } else {
+     e.preventDefault();
      return false;
    }
 },false);
